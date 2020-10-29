3e0118a75aa7bd0c6d5e02b5a170d65070d557ca
Tommy McNeely <thomas.mcneely@davita.com>
2020-10-29 11:17:37 -0600

    fix: delete_board improvements...
    * Output improvements
    * Reduce duplicate "Delete:" statements
    * "handle" 401 Unauthorized differently (joined boards)
    * Add --debug to print board object
    * Added a README :)

---
+++ delete_boards/README.md
@@ -0,0 +1,48 @@
+# Golang Trello Example - Delete Boards
+
+go-trello-example/delete_boards is a [Go](http://golang.org/) *useful* example client package for accessing the [Trello](http://www.trello.com/) [API](https://developer.atlassian.com/cloud/trello/guides/rest-api/api-introduction/) using the [TJM/go-trello](https://github.com/TJM/go-trello) module to delete trello boards that match specific conditions
+
+## Example
+
+Prerequisites:
+
+* Retrieve your `appKey`: <https://trello.com/app-key> (NOTE: This identifies "you" as the developer of the application)
+* Retrieve your (temporary) `token`: <https://trello.com/1/connect?key=MYKEYFROMABOVE&name=MYAPPNAME&response_type=token&scope=read,write&expiration=1day>
+
+Building:
+
+```bash
+go build
+```
+
+Running: (using your appKey and token from above)
+
+```bash
+export TRELLO_APP_KEY=xxxxxxxxxxxxxxxxxxx
+export TRELLO_TOKEN=yyyyyyyyyyyyyyyyyyyy
+./delete_boards --help
+Usage: delete_boards --appkey APPKEY --token TOKEN [--anyof] [--startswith STARTSWITH] [--contains CONTAINS] [--endswith ENDSWITH] [--delete] [--debug]
+
+Options:
+  --appkey APPKEY        Trello API App Key.
+         Obtain yours at https://trello.com/app-key
+         (env: TRELLO_APP_KEY)
+  --token TOKEN          Trello API App Key.
+         Authorize your App Key to use your account at <https://trello.com/1/connect?key=<appKey from above>&name=Go-Trello-Example-delete_boards&response_type=token&scope=read,write&expiration=1day>
+         (env: TRELLO_TOKEN)
+  --anyof                Match AnyOf the StartsWith, Contains or EndsWith conditions. By default board name must match all of the conditions.
+  --startswith STARTSWITH
+                         Select boards to delete that *start with* this string
+  --contains CONTAINS    Select boards to delete that *contain* this string
+  --endswith ENDSWITH    Select boards to delete that *end with* this string
+  --delete               Actually DELETE the boards (defaults to false so you can see what will happen)
+  --debug                Enable debugging output
+  --help, -h             display this help and exit
+
+```
+
+NOTE: The "--delete" flag is required to *actually* delete boards, so you can safely test this.
+
+## License
+
+Licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
--- delete_boards/delete_boards.go
+++ delete_boards/delete_boards.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -10,13 +11,14 @@ import (
 )
 
 var args struct {
-	AppKey     string `arg:"required,env:TRELLO_APP_KEY" help:"Trello API App Key, Obtain yours at https://trello.com/app-key ... (env: TRELLO_APP_KEY)"`
-	Token      string `arg:"required,env:TRELLO_TOKEN" help:"Trello API App Key, Authorize your App Key to use your account at <https://trello.com/1/connect?key=<appKey from above>&name=Go-Trello-Example-delete-boards&response_type=token&scope=read,write&expiration=1day> (env: TRELLO_TOKEN)"`
+	AppKey     string `arg:"required,env:TRELLO_APP_KEY" help:"Trello API App Key.\n\t\t\t\t Obtain yours at https://trello.com/app-key\n\t\t\t\t (env: TRELLO_APP_KEY)"`
+	Token      string `arg:"required,env:TRELLO_TOKEN" help:"Trello API App Key.\n\t\t\t\t Authorize your App Key to use your account at <https://trello.com/1/connect?key=<appKey from above>&name=Go-Trello-Example-delete_boards&response_type=token&scope=read,write&expiration=1day>\n\t\t\t\t (env: TRELLO_TOKEN)"`
 	AnyOf      bool   `help:"Match AnyOf the StartsWith, Contains or EndsWith conditions. By default board name must match all of the conditions."`
 	StartsWith string `help:"Select boards to delete that *start with* this string"`
 	Contains   string `help:"Select boards to delete that *contain* this string"`
 	EndsWith   string `help:"Select boards to delete that *end with* this string"`
 	Delete     bool   `help:"Actually DELETE the boards (defaults to false so you can see what will happen)"`
+	Debug      bool   `help:"Enable debugging output"`
 }
 
 func main() {
@@ -47,19 +49,23 @@ func main() {
 	fmt.Printf("Trello Boards: %v\n", len(user.IDBoards))
 
 	for _, board := range boards {
+		if args.Debug {
+			boardJSON, err := json.MarshalIndent(board, "", "  ")
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+			fmt.Printf("====================\n%s:\n%s\n\n", board.Name, string(boardJSON))
+		}
 		if args.AnyOf {
 			// AnyOf
 			if args.StartsWith != "" && strings.HasPrefix(board.Name, args.StartsWith) {
-				fmt.Println("Delete: " + board.Name)
 				removeBoard(board)
 			} else if args.Contains != "" && strings.Contains(board.Name, args.Contains) {
-				fmt.Println("Delete: " + board.Name)
 				removeBoard(board)
 			} else if args.EndsWith != "" && strings.HasSuffix(board.Name, args.EndsWith) {
-				fmt.Println("Delete: " + board.Name)
 				removeBoard(board)
 			} else {
-				fmt.Println("Keep: " + board.Name)
+				fmt.Println("Keep:   " + board.Name)
 			}
 		} else {
 			if args.StartsWith == "" && args.Contains == "" && args.EndsWith == "" { // If no conditions are set, KEEP
@@ -67,24 +73,31 @@ func main() {
 			} else if (args.StartsWith == "" || strings.HasPrefix(board.Name, args.StartsWith)) &&
 				(args.Contains == "" || strings.Contains(board.Name, args.Contains)) &&
 				(args.EndsWith == "" || strings.HasSuffix(board.Name, args.EndsWith)) { // If a condition is set, and matches, DELETE
-				fmt.Println("Delete: " + board.Name)
 				removeBoard(board)
 			} else { // KEEP by default
-				fmt.Println("Keep: " + board.Name)
+				fmt.Println("Keep:   " + board.Name)
 			}
 		}
 	}
 
 	if !args.Delete {
-		fmt.Printf("\n\n ** Run again with --delete flag to actually delete the boards.\n\n")
+		fmt.Printf("\n\n ** Run again with --delete flag to actually delete the board(s).\n\n")
 	}
 }
 
 func removeBoard(board trello.Board) {
 	if args.Delete {
+		fmt.Println("DELETING: '" + board.Name)
 		err := board.Delete()
 		if err != nil {
-			fmt.Printf("ERROR Deleting board: %v\n", err)
+			if strings.Contains(err.Error(), "401") { // UNAUTHORIZED (try to leave instead)
+				fmt.Println("Leaving ORG Trello Board: '" + board.Name)
+				fmt.Println("NOTE: board.RemoveMember is not yet implemented! Coming soon?")
+			} else {
+				fmt.Printf("ERROR Deleting board: %v\n", err)
+			}
 		}
+	} else {
+		fmt.Println("Delete: " + board.Name)
 	}
 }
